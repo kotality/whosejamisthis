243caa92caef91285d0b8ba59628ce13b0430c41
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-09-01 15:12:07 -0400

    Revert "Merge branch 'Sahil' into Fornan"
    This reverts commit 3b6f6e5ee816ec515a518e3412248a97b7910f6b, reversing
    changes made to 4a42bd04f965b3be7d8b2075a38b4a074f59a787.

---
+++ Assets/Sahil/Character/State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Reflection;
+
+[CreateAssetMenu(fileName = "New State", menuName = "AI/State")]
+public class State : ScriptableObject {
+    public float speed;
+
+    public Behaviors onStartEvent;
+    public UnityEvent onTickEvent;
+    public UnityEvent onExitEvent;
+
+    public State[] nextStates;
+
+    public virtual bool StateCriteriaMet()
+    {
+        return true;
+    }
+
+    public virtual void OnStart()
+    {
+    }
+    public virtual void OnTick()
+    {
+
+    }
+    public virtual void OnExit()
+    {
+
+    }
+}
