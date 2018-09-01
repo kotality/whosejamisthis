18d4e49a3247601572d69497419088297d687b6c
Sahil <sahilpohare@gmail.com>
2018-09-01 21:59:05 +0530

    kjnkjn

---
+++ Assets/Sahil/Character/State.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : ScriptableObject {
+    public float speed;
+    
+    public virtual void OnStart()
+    {
+
+    }
+    public virtual void OnTick()
+    {
+
+
+    }
+}
---
+++ Assets/Sahil/Character/StatesManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatesManager : MonoBehaviour {
+    public State currentState;
+    public Dictionary<string,State> Statelist;
+    public string[] a;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
