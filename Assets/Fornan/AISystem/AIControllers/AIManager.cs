e5d8c4d4aaa564b63955147962c18839be3af601
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-09-01 16:26:28 -0400

    AIManager WIP

---
+++ Assets/Fornan/AISystem/AIControllers/AIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIManager : MonoBehaviour {
+
+    protected NavMeshAgent _myAgent;
+    protected StatesManager _sm;
+
+    public bool isControllingCreature = true;
+
+    public GameObject creatureToBeAngryAt = null;
+
+    public State ThinkState;
+    public State AngryState;
+    public State IdleState;
+    public State PatrolState;
+    public State AttackState;
+
+    protected virtual void Start()
+    {
+        _myAgent = gameObject.GetComponent<NavMeshAgent>();
+        _sm = gameObject.GetComponent<StatesManager>();
+
+        if(isControllingCreature)
+        {
+            _sm.DoTransition(ThinkState);
+        }
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+}
