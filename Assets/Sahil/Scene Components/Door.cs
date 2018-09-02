6671a440332f8984f0543e3b456d6ab055badd02
Sahil <sahilpohare@gmail.com>
2018-09-03 00:50:09 +0530

    s

---
+++ Assets/Sahil/Scene Components/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Activable {
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    public override void OnActivate()
+    {
+        base.OnActivate();
+        anim.SetBool("Open", true);
+    }
+    public override void DeActivate()
+    {
+        base.DeActivate();
+        anim.SetBool("Open", false);
+    }
+}
---
+++ Assets/Sahil/Scene Components/TriggerActivator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class TriggerActivator : MonoBehaviour {
+    UnityEvent onEnter;
+    UnityEvent onExit;
+    public AIManager.CreatureType creatureType;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<AIManager>() != null) { 
+        if (!other.GetComponent<AIManager>().isControllingCreature)
+        {
+                if (other.GetComponent<AIManager>().myDamageType == creatureType)
+                {
+                    onEnter.Invoke();
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.GetComponent<AIManager>() != null) {
+            if (!other.GetComponent<AIManager>().isControllingCreature)
+            {
+                if (other.GetComponent<AIManager>().myDamageType == creatureType)
+                {
+                    onExit.Invoke();
+                }
+            }
+        }
+    }
+}
