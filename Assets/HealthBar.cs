83b034f368ae625ccef69f35567b986a3966d5cf
Sahil <sahilpohare@gmail.com>
2018-09-03 02:14:47 +0530

    MainMenu

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour {
+    public DamageReciever d;
+
+	// Update is called once per frame
+	void Update () {
+       transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().fillAmount = (int)(d.currentHealth / d.maxHealth);
+	}
+}
