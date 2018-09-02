583da8ddcc7606bcc323404611a474e6cd40ea47
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-09-02 15:50:02 -0400

    FireBoi Done

---
+++ Assets/Fornan/AISystem/RangedDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedDamage : DamageEmitter {
+
+    public float attackCoolDown = 2.0f;
+    public GameObject projectile;
+
+    public override void UseAttack(GameObject recipient)
+    {
+        Debug.Log("ATTACK");
+        if(currentlyAttacking) { return; }
+
+        _anim.SetTrigger("Attack");
+        GameObject proj = Instantiate(projectile, transform.position, transform.rotation);
+        proj.GetComponent<Projectile>().de = this;
+        StartCoroutine(RunCoolDown());
+    }
+
+    protected virtual IEnumerator RunCoolDown()
+    {
+        currentlyAttacking = true;
+        float timer = attackCoolDown;
+        while(timer > 0)
+        {
+            yield return null;
+            timer -= Time.deltaTime;
+        }
+        currentlyAttacking = false;
+    }
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float LifeTime = 4.0f;
+    public float Velocity = 3.0f;
+    public DamageEmitter de;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb.velocity = transform.forward * Velocity;
+        Destroy(gameObject, LifeTime);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == de.owner) { return; }
+
+        DamageReciever dr = collision.gameObject.GetComponent<DamageReciever>();
+        if(dr)
+        {
+            dr.TakeDamageFrom(de);
+        }
+
+        Destroy(gameObject);
+    }
+}
